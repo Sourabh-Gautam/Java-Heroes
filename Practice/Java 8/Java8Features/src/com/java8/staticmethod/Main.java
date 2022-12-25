d57aadfecea0cc15058528173dddf8e71eed911d
Sourabh Gautam <sourabh_gautam@epam.com>
2022-12-25 22:23:50 +0530

    Default and static methods in interfaces completed with notes

---
+++ Practice/Java 8/Java8Features/src/com/java8/staticmethod/Main.java
@@ -0,0 +1,11 @@
+package com.java8.staticmethod;
+
+public interface Main  {
+
+	public static void main(String[] args) {
+		
+		MyUtil.sort();
+		
+	}
+	
+}
---
+++ Practice/Java 8/Java8Features/src/com/java8/staticmethod/MyUtil.java
@@ -0,0 +1,17 @@
+package com.java8.staticmethod;
+
+public interface MyUtil {
+	
+	static void sort() {
+		System.out.println("Sorting");
+	}
+	
+	static void reverse() {
+		System.out.println("Reversing");
+	}
+	
+	static void count() { 
+		System.out.println("Counting");
+	}
+	
+}
